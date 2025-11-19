0a19fdb65c38aafec5f1927b331e8d569703bfa9
rysbrand <becca.leach325@gmail.com>
2025-11-19 16:44:49 -0600

    working on debugging and testing

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
 session_start();
-require_once('models/datebase.php');
+require_once('models/database.php');
 require_once('models/fields.php');
 require_once('models/validate.php');
-
-if (!isset($_SESSION['is_valid_admin'])) {
-    $action = 'login';
-}
-
+require_once('models/users_db.php');
 
 $validate = new Validate();
 $fields = $validate->getFields();
@@ -20,33 +16,67 @@ $fields->addField('last_name');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
-    $action = 'reset';
-} else {
+    $action = filter_input(INPUT_GET, 'action');
+}
+
+if ($action === NULL) {
+    if (empty($_SESSION['user_id'])) {
+        $action = 'show_login';
+    } 
+    else {
+        $action = 'show_dashboard';
+    }
+} 
+else {
     $action = strtolower($action);
 }
 
 switch ($action) {
+
+    case 'show_login':
+        $email = '';
+        $password = '';
+        $login_message = $login_message ?? '';
+        include 'views/login/login.php';
+        break;
+
     case 'login':
         $email = filter_input(INPUT_POST, 'email');
         $password = filter_input(INPUT_POST, 'password');
-        if (is_valid_admin_login($email, $password)) {
-            $_SESSION['is_valid_admin'] = true;
-            include('view/admin_menu.php');
+
+        if (is_valid_user_login($email, $password)) {
+
+            $user = get_user_by_email($email);
+
+            $_SESSION['user_id'] = $user['userID'];
+            $_SESSION['user_email'] = $email['emailAddress'];
+
+            include 'views/home/dashboard.php';
         } else {
-            $login_message = 'You must login to view this page.';
-            include('view/login.php');
+            $login_message = 'Invalid email or password.';
+            include 'views/login/login.php';
         }
         break;
-    case 'Reset':
+
+    case 'reset':
+        $email = '';
+        $password = '';
+        $verify = '';
+        $firstName = '';
+        $lastName = '';       
+        include 'views/login/register.php';
+        break;
+
+    case 'show_register':
         $email = '';
         $password = '';
         $verify = '';
         $firstName = '';
         $lastName = '';
-        
-        include 'views/login/register.php';
+        include 'views\login\register.php';
         break;
-    case 'Register':
+
+    case 'register':
         // Copy form values to local variables
         $email = trim(filter_input(INPUT_POST, 'email'));
         $password = filter_input(INPUT_POST, 'password');
@@ -60,21 +90,26 @@ switch ($action) {
         $validate->verify('verify', $password, $verify);
         $validate->text('first_name', $firstName);
         $validate->text('last_name', $lastName);
+
+        add_user($email, $password, $firstName, $lastName);
+
+        include 'views/login/success.php';
+        break;
+
+    case 'show_dashboard':
+        if (empty($_SESSION['user_id'])) {
+            $login_message = 'Please login first.';
+            include 'views/login/login.php';
+        } else {
+            include 'views/home/dashboard.php';
+        }
+        break;
+
     case 'logout':
         $_SESSION = array();   // Clear all session data from memory
         session_destroy();     // Clean up the session ID
         $login_message = 'You have been logged out.';
-        include('view/login.php');
+        include('views/login/login.php');
         break;
-
-
-        // Load appropriate view based on hasErrors
-        if ($fields->hasErrors()) {
-            include 'views/login/register.php';
-        } 
-        else {
-            include 'views/login/success.php';
-        }
-        break;
-}
+    }
 ?>
