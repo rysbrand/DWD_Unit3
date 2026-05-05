a369ca90e20f5b06bd8acb559c7213a5c7e80ee3
Rebecca Ysbrand <becca.leach325@gmail.com>
2026-05-05 20:25:44 +0000

    updated database.php to use infinity free

--- models/database.php
+++ models/database.php
@@ -1,14 +1,14 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=dwd_unit3db';
-$username = 'root';
-$password = '';
+$dsn = 'mysql:host=sql306.infinityfree.com;dbname=if0_41840821_dwd_unit3db';
+$username = 'if0_41840821';
+$password = 'YOUR_MYSQL_PASSWORD_HERE';
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 try {
     $db = new PDO($dsn, $username, $password, $options);
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
-    include('views/errors/db_error_connect.php');
+    include('errors/db_error_connect.php');
     exit();
 }
 ?>
